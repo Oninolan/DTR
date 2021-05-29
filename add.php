a1f5a4341ccc8026417421e5eed8c76ec5aa250d
Oninolan <61404230+Oninolan@users.noreply.github.com>
2021-05-29 22:49:51 +0800

    DTR page design update

--- add.php
+++ add.php
@@ -1,12 +1,15 @@
 <link rel="stylesheet" href="style.css">
 <div class = "logincentered">
+
 <form action ="add.php" method = "post">
+	<p style="color:white">
 	Enter ID: <input type="text" name= "eid" size="8" /> <br />
 	Enter Name: <input type="text" name= "ename" size="50" /> <br />
 	Enter Status: <input type="text" name= "estatus" size="8" /> <br />
 	Enter Gender: <input type="text" name= "egender" size="50" /> <br />
 	Enter Username: <input type="text" name= "euser" size="8" /> <br />
 	Enter Password: <input type="password" name= "epass" size="50" /> <br />
+	</p>
 	
 	<center><input type ="submit" name ="Save" class="button"/><center> <br />
 </form>
